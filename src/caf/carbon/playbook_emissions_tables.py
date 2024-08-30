e59f70adcc647a06c6db708a33865734b81e8a82
Matt Buckley <matt.buckley@transportforthenorth.com>
2024-08-30 11:11:42 +0100

    Format playbook emissions

--- src/caf/carbon/playbook_emissions_tables.py
+++ src/caf/carbon/playbook_emissions_tables.py
@@ -15,12 +15,14 @@ class PlaybookProcess:
         self.emissions_file = emissions_file
         self.working_directory = "D:/GitHub/caf.carbon/CAFCarb/outputs/"
         self.area_type_lookup = pd.read_csv(
-            r"G:\raw_data\WSP Carbon Playbook\WSP_final\msoa_reclassifications_england_v6.csv")
-        self.area_type_lookup = self.area_type_lookup[["MSOA11CD", "RUC_Class"]].rename(columns={
-            "MSOA11CD": "origin",
-            "RUC_Class": "Origin Place Type"})
+            r"G:\raw_data\WSP Carbon Playbook\WSP_final\msoa_reclassifications_england_v6.csv"
+        )
+        self.area_type_lookup = self.area_type_lookup[["MSOA11CD", "RUC_Class"]].rename(
+            columns={"MSOA11CD": "origin", "RUC_Class": "Origin Place Type"}
+        )
         self.code_lookup = pd.read_csv(
-            r"A:\QCR- assignments\03.Assignments\h5files\Other Inputs\North_through_lookup-MSOA11_lta.csv")
+            r"A:\QCR- assignments\03.Assignments\h5files\Other Inputs\North_through_lookup-MSOA11_lta.csv"
+        )
 
         self.run_analysis()
 
@@ -29,10 +31,15 @@ class PlaybookProcess:
         for time_period in self.time_periods:
             emissions_data = pd.read_hdf(
                 f"{self.working_directory}/TfN_carbon_playbook_{scenario}"
-                f"_fleet_emissions_{self.out_date}.h5", f"{time_period}_{year}", mode='r')
+                f"_fleet_emissions_{self.out_date}.h5",
+                f"{time_period}_{year}",
+                mode="r",
+            )
             emissions_data["time_period"] = time_period
             year_emissions_data = pd.concat([year_emissions_data, emissions_data])
-        through_lta = self.code_lookup[["lad", "through_name"]].rename(columns={"lad": "through"})
+        through_lta = self.code_lookup[["lad", "through_name"]].rename(
+            columns={"lad": "through"}
+        )
         year_emissions_data = year_emissions_data.merge(through_lta, how="left", on="through")
         year_emissions_data.drop(columns=["through"])
         year_emissions_data = year_emissions_data.rename(columns={"through_name": "through"})
@@ -47,22 +54,37 @@ class PlaybookProcess:
 
             day_data.loc[day_data["vehicle_type"] == "car", demand] = day_data[demand] * 348
             day_data.loc[day_data["vehicle_type"] == "lgv", demand] = day_data[demand] * 329
-            day_data.loc[day_data["vehicle_type"] == "hgv", demand] = day_data[demand] * 297 / 2.5  # PCU accounting
+            day_data.loc[day_data["vehicle_type"] == "hgv", demand] = (
+                day_data[demand] * 297 / 2.5
+            )  # PCU accounting
 
         unsimulated_data = day_data.copy()
         for demand in ["tailpipe_gco2", "grid_gco2", "vkm"]:
-            unsimulated_data.loc[unsimulated_data["vehicle_type"] == "car", demand] = (unsimulated_data[demand] * 3
-                                                                                       * 0.238)
-            unsimulated_data.loc[unsimulated_data["vehicle_type"] == "lgv", demand] = (unsimulated_data[demand] * 6
-                                                                                       * 0.216)
-            unsimulated_data.loc[unsimulated_data["vehicle_type"] == "hgv", demand] = (unsimulated_data[demand] * 3
-                                                                                       * 331)
+            unsimulated_data.loc[unsimulated_data["vehicle_type"] == "car", demand] = (
+                unsimulated_data[demand] * 3 * 0.238
+            )
+            unsimulated_data.loc[unsimulated_data["vehicle_type"] == "lgv", demand] = (
+                unsimulated_data[demand] * 6 * 0.216
+            )
+            unsimulated_data.loc[unsimulated_data["vehicle_type"] == "hgv", demand] = (
+                unsimulated_data[demand] * 3 * 331
+            )
         unsimulated_data["time_period"] = "TS4"
         year_data = pd.concat([day_data, unsimulated_data])
         year_data = (
             year_data.groupby(
-                ["origin", "destination", "through", "fuel", "user_class", "vehicle_type",
-                 "year", "trip_band", "time_period", "tally"]
+                [
+                    "origin",
+                    "destination",
+                    "through",
+                    "fuel",
+                    "user_class",
+                    "vehicle_type",
+                    "year",
+                    "trip_band",
+                    "time_period",
+                    "tally",
+                ]
             )[["tailpipe_gco2", "grid_gco2", "vkm"]]
             .sum()
             .reset_index()
@@ -71,7 +93,9 @@ class PlaybookProcess:
 
     def create_2019_purpose(self, annual_data):
         purpose_data = annual_data[["through", "user_class", "tailpipe_gco2", "grid_gco2"]]
-        purpose_data["Emissions (tCO2e)"] = purpose_data["tailpipe_gco2"] + purpose_data["grid_gco2"]
+        purpose_data["Emissions (tCO2e)"] = (
+            purpose_data["tailpipe_gco2"] + purpose_data["grid_gco2"]
+        )
         purpose_data["Emissions (tCO2e)"] = purpose_data["Emissions (tCO2e)"] / (1000 * 1000)
         purpose_data = purpose_data.rename(columns={"through": "LTA"})
         purpose_data.loc[purpose_data["user_class"] == "uc1", "Purpose"] = "Business"
@@ -79,21 +103,31 @@ class PlaybookProcess:
         purpose_data.loc[purpose_data["user_class"] == "uc3", "Purpose"] = "Other"
         purpose_data.loc[purpose_data["user_class"] == "uc4", "Purpose"] = "LGV"
         purpose_data.loc[purpose_data["user_class"] == "uc5", "Purpose"] = "HGV"
-        purpose_data = purpose_data[["LTA", "Purpose", "Emissions (tCO2e)"]].groupby(
-            ["LTA", "Purpose"], as_index=False).sum()
+        purpose_data = (
+            purpose_data[["LTA", "Purpose", "Emissions (tCO2e)"]]
+            .groupby(["LTA", "Purpose"], as_index=False)
+            .sum()
+        )
         # interpolation
         return purpose_data
 
     def create_vehicle_type(self, annual_data):
         vehicle_data = annual_data[["through", "vehicle_type", "tailpipe_gco2", "grid_gco2"]]
-        vehicle_data["Emissions (tCO2e)"] = vehicle_data["tailpipe_gco2"] + vehicle_data["grid_gco2"]
+        vehicle_data["Emissions (tCO2e)"] = (
+            vehicle_data["tailpipe_gco2"] + vehicle_data["grid_gco2"]
+        )
         vehicle_data["Emissions (tCO2e)"] = vehicle_data["Emissions (tCO2e)"] / (1000 * 1000)
-        vehicle_data = vehicle_data.rename(columns={"through": "LTA", "vehicle_type": "Vehicle"})
+        vehicle_data = vehicle_data.rename(
+            columns={"through": "LTA", "vehicle_type": "Vehicle"}
+        )
         vehicle_data.loc[vehicle_data["vehicle_type"] == "car", "vehicle_type"] = "Car"
         vehicle_data.loc[vehicle_data["vehicle_type"] == "lgv", "vehicle_type"] = "LGV"
         vehicle_data.loc[vehicle_data["vehicle_type"] == "hgv", "vehicle_type"] = "HGV"
-        vehicle_data = vehicle_data[["LTA", "Vehicle", "Emissions (tCO2e)"]].groupby(
-            ["LTA", "Vehicle"], as_index=False).sum()
+        vehicle_data = (
+            vehicle_data[["LTA", "Vehicle", "Emissions (tCO2e)"]]
+            .groupby(["LTA", "Vehicle"], as_index=False)
+            .sum()
+        )
         # interpolation
         return vehicle_data
 
@@ -103,50 +137,82 @@ class PlaybookProcess:
         type_data["Emissions (tCO2e)"] = type_data["tailpipe_gco2"] + type_data["grid_gco2"]
         type_data["Emissions (tCO2e)"] = type_data["Emissions (tCO2e)"] / (1000 * 1000)
         type_data = type_data.rename(columns={"through": "LTA"})
-        type_data = type_data[["LTA", "Origin Place Type", "Emissions (tCO2e)"]].groupby(
-            ["LTA", "Origin Place Type"], as_index=False).sum()
+        type_data = (
+            type_data[["LTA", "Origin Place Type", "Emissions (tCO2e)"]]
+            .groupby(["LTA", "Origin Place Type"], as_index=False)
+            .sum()
+        )
         # interpolation
         return type_data
 
     def create_trip_length(self, annual_data):
-        trip_data = annual_data[["origin", "destination", "through", "trip_band", "tailpipe_gco2", "grid_gco2"]]
+        trip_data = annual_data[
+            ["origin", "destination", "through", "trip_band", "tailpipe_gco2", "grid_gco2"]
+        ]
         trip_data["Emissions"] = trip_data["tailpipe_gco2"] + trip_data["grid_gco2"]
         trip_data["Emissions"] = trip_data["Emissions"] / (1000 * 1000)
         trip_data = trip_data.rename(columns={"through": "LTA", "trip_band": "Trip Length"})
-        trip_data = trip_data[["LTA", "Trip Length", "Emissions"]].groupby(
-            ["LTA", "Trip Length"], as_index=False).sum()
+        trip_data = (
+            trip_data[["LTA", "Trip Length", "Emissions"]]
+            .groupby(["LTA", "Trip Length"], as_index=False)
+            .sum()
+        )
         # interpolation
         return trip_data
 
     def create_genesis(self, annual_data):
-        genesis_data = annual_data[["origin", "destination", "through", "trip_band", "tailpipe_gco2", "grid_gco2"]]
-        origin_lta = self.code_lookup[["zone_cd", "through_name"]].rename(columns={"zone_cd": "origin",
-                                                                                   "through_name": "o_lta"})
+        genesis_data = annual_data[
+            ["origin", "destination", "through", "trip_band", "tailpipe_gco2", "grid_gco2"]
+        ]
+        origin_lta = self.code_lookup[["zone_cd", "through_name"]].rename(
+            columns={"zone_cd": "origin", "through_name": "o_lta"}
+        )
         genesis_data = genesis_data.merge(origin_lta, how="left", on="origin")
-        destination_lta = self.code_lookup[["zone", "through_name"]].rename(columns={"zone": "destination",
-                                                                                   "through_name": "d_lta"})
+        destination_lta = self.code_lookup[["zone", "through_name"]].rename(
+            columns={"zone": "destination", "through_name": "d_lta"}
+        )
         genesis_data = genesis_data.merge(destination_lta, how="left", on="destination")
-        genesis_data["Emissions (tCO2e)"] = genesis_data["tailpipe_gco2"] + genesis_data["grid_gco2"]
+        genesis_data["Emissions (tCO2e)"] = (
+            genesis_data["tailpipe_gco2"] + genesis_data["grid_gco2"]
+        )
         genesis_data["Emissions (tCO2e)"] = genesis_data["Emissions (tCO2e)"] / (1000 * 1000)
         genesis_data = genesis_data[["o_lta", "d_lta", "t_lta", "Emissions"]]
         genesis_data["Genesis"] = "Through"
-        genesis_data.loc[(genesis_data["o_lta"] == genesis_data["d_lta"]) &
-                         (genesis_data["o_lta"] == genesis_data["through"]), "Genesis"] = "Internal"
-        genesis_data.loc[(genesis_data["o_lta"] == genesis_data["through"]) &
-                         (genesis_data["d_lta"] != genesis_data["through"]), "Genesis"] = "Outbound"
-        genesis_data.loc[(genesis_data["d_lta"] == genesis_data["through"]) &
-                         (genesis_data["o_lta"] != genesis_data["through"]), "Genesis"] = "Inbound"
+        genesis_data.loc[
+            (genesis_data["o_lta"] == genesis_data["d_lta"])
+            & (genesis_data["o_lta"] == genesis_data["through"]),
+            "Genesis",
+        ] = "Internal"
+        genesis_data.loc[
+            (genesis_data["o_lta"] == genesis_data["through"])
+            & (genesis_data["d_lta"] != genesis_data["through"]),
+            "Genesis",
+        ] = "Outbound"
+        genesis_data.loc[
+            (genesis_data["d_lta"] == genesis_data["through"])
+            & (genesis_data["o_lta"] != genesis_data["through"]),
+            "Genesis",
+        ] = "Inbound"
 
         genesis_data = genesis_data.rename(columns={"through": "LTA"})
-        genesis_data = genesis_data[["LTA", "Genesis", "Emissions (tCO2e)"]].groupby(
-            ["LTA", "Trip Length"], as_index=False).sum()
+        genesis_data = (
+            genesis_data[["LTA", "Genesis", "Emissions (tCO2e)"]]
+            .groupby(["LTA", "Trip Length"], as_index=False)
+            .sum()
+        )
         return genesis_data
 
     def vkm_info(self, data):
-        data = data[["through", "year", "tailpipe_gco2", "grid_gco2"]].groupby(
-            ["through", "year"], as_index=False).sum()
-        data_totals = data[["through", "year", "vehicle_type", "vkm"]].groupby(
-            ["through", "year", "vehicle_type"], as_index=False).sum()
+        data = (
+            data[["through", "year", "tailpipe_gco2", "grid_gco2"]]
+            .groupby(["through", "year"], as_index=False)
+            .sum()
+        )
+        data_totals = (
+            data[["through", "year", "vehicle_type", "vkm"]]
+            .groupby(["through", "year", "vehicle_type"], as_index=False)
+            .sum()
+        )
         data_totals = data_totals.rename(columns={"vkm": "vkm_total"})
         data.loc[data["fuel"] == "hydrogen", "fuel"] = "ZEV"
         data.loc[data["fuel"] == "bev", "fuel"] = "ZEV"
@@ -154,19 +220,30 @@ class PlaybookProcess:
         data = data.merge(data_totals, how="left", on=["through", "year", "vehicle_type"])
         data["zev mileage"] = data["vkm"] / data["vkm_total"]
         data = data.rename(columns={"through": "LTA"})
-        data = data[["LTA", "year", "zev mileage", "vehicle_type"]].groupby(
-            ["LTA", "year", "vehicle_type"], as_index=False).sum()
-        transformed_data = data.pivot(index=["LTA", "year"], columns="vehicle_type", values="zev_mileage")
+        data = (
+            data[["LTA", "year", "zev mileage", "vehicle_type"]]
+            .groupby(["LTA", "year", "vehicle_type"], as_index=False)
+            .sum()
+        )
+        transformed_data = data.pivot(
+            index=["LTA", "year"], columns="vehicle_type", values="zev_mileage"
+        )
         return transformed_data
 
     def co2_info(self, data):
-        data = data[["through", "year", "tailpipe_gco2", "grid_gco2"]].groupby(
-            ["through", "year", "vehicle_type", "fuel_type"], as_index=False).sum()
+        data = (
+            data[["through", "year", "tailpipe_gco2", "grid_gco2"]]
+            .groupby(["through", "year", "vehicle_type", "fuel_type"], as_index=False)
+            .sum()
+        )
         data["Emissions (MtCO2e)"] = data["tailpipe_gco2"] + data["grid_gco2"]
         data["Emissions (MtCO2e)"] = data["Emissions (MtCO2e)"] / (1000 * 1000 * 1000 * 1000)
         data = data.rename(columns={"through": "LTA"})
-        data = data[["LTA", "year", "Emissions (MtCO2e)"]].groupby(
-            ["LTA", "year"], as_index=False).sum()
+        data = (
+            data[["LTA", "year", "Emissions (MtCO2e)"]]
+            .groupby(["LTA", "year"], as_index=False)
+            .sum()
+        )
         return data
 
     def create_vkm_splits(self, data_2018, data_2028, data_2038, data_2043, data_2048):
@@ -230,13 +307,17 @@ class PlaybookProcess:
         return co2_emission_data
 
     def lininterpol(self, df1, df2, year1, year2, targetyear, columns):
-        """ Conduct the Interpolation """
+        """Conduct the Interpolation"""
         df1year = year1
         df2year = year2
         year = targetyear
         target = df1.copy()
         for i in columns:
-            target[i] = -1 * ((df2year - year) * df1[i] + (year - df1year) * df2[i]) / (df1year - df2year)
+            target[i] = (
+                -1
+                * ((df2year - year) * df1[i] + (year - df1year) * df2[i])
+                / (df1year - df2year)
+            )
         target["year"] = targetyear
         return target
 
@@ -247,8 +328,14 @@ class PlaybookProcess:
         emissions_2028 = self.read_cafcarb_output(2028, "bau")
         emissions_2028 = self.apply_annualisation(emissions_2028)
 
-        emissions_2019 = self.lininterpol(emissions_2018, emissions_2028, 2018, 2028, 2019,
-                                          ["tailpipe_gco2", "grid_gco2", "vkm"])
+        emissions_2019 = self.lininterpol(
+            emissions_2018,
+            emissions_2028,
+            2018,
+            2028,
+            2019,
+            ["tailpipe_gco2", "grid_gco2", "vkm"],
+        )
 
         purpose_2019 = self.create_2019_purpose(emissions_2019)
         purpose_2019.to_csv("2019 Purpose.csv")
@@ -274,17 +361,21 @@ class PlaybookProcess:
         emissions_2048 = self.read_cafcarb_output(2048, "bau")
         emissions_2048 = self.apply_annualisation(emissions_2048)
 
-        emissions_2050 = self.lininterpol(emissions_2043, emissions_2048, 2043, 2048, 2050,
-                                          ["tailpipe_gco2", "grid_gco2", "vkm"])
+        emissions_2050 = self.lininterpol(
+            emissions_2043,
+            emissions_2048,
+            2043,
+            2048,
+            2050,
+            ["tailpipe_gco2", "grid_gco2", "vkm"],
+        )
 
         vehicle_2050 = self.create_vehicle_type(emissions_2050)
         vehicle_2050.to_csv("2050 Vehicle.csv")
 
-        vkm_splits_bau = self.create_vkm_splits(emissions_2018,
-                                            emissions_2028,
-                                            emissions_2038,
-                                            emissions_2043,
-                                            emissions_2048)
+        vkm_splits_bau = self.create_vkm_splits(
+            emissions_2018, emissions_2028, emissions_2038, emissions_2043, emissions_2048
+        )
         vkm_splits_bau.to_csv("vkm_splits_bau.csv")
 
         ue_emissions_2018 = self.read_cafcarb_output(2018, "cas")
@@ -298,23 +389,25 @@ class PlaybookProcess:
         ue_emissions_2048 = self.read_cafcarb_output(2048, "cas")
         ue_emissions_2048 = self.apply_annualisation(ue_emissions_2048)
 
-        vkm_splits_ue = self.create_vkm_splits(ue_emissions_2018,
-                                                ue_emissions_2028,
-                                                ue_emissions_2038,
-                                                ue_emissions_2043,
-                                                ue_emissions_2048)
+        vkm_splits_ue = self.create_vkm_splits(
+            ue_emissions_2018,
+            ue_emissions_2028,
+            ue_emissions_2038,
+            ue_emissions_2043,
+            ue_emissions_2048,
+        )
         vkm_splits_ue.to_csv("vkm_splits_ue.csv")
 
-        co2_emissions = self.create_co2emissions(emissions_2018,
-                                                    emissions_2028,
-                                                    emissions_2038,
-                                                    emissions_2043,
-                                                    emissions_2048)
+        co2_emissions = self.create_co2emissions(
+            emissions_2018, emissions_2028, emissions_2038, emissions_2043, emissions_2048
+        )
         co2_emissions.to_csv("co2_emissions_bau.csv")
 
-        co2_emissions_ue = self.create_co2emissions(ue_emissions_2018,
-                                               ue_emissions_2028,
-                                               ue_emissions_2038,
-                                               ue_emissions_2043,
-                                               ue_emissions_2048)
+        co2_emissions_ue = self.create_co2emissions(
+            ue_emissions_2018,
+            ue_emissions_2028,
+            ue_emissions_2038,
+            ue_emissions_2043,
+            ue_emissions_2048,
+        )
         co2_emissions_ue.to_csv("co2_emissions_ue.csv")
